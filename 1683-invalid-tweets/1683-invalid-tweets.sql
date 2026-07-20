8aba1fac8ca2a30a5dc1b906411e966b5adc3832
Pranavagg140 <63508046+Pranavagg140@users.noreply.github.com>
2026-07-20 15:40:08 +0530

    Time: 664 ms (27.75%), Space: 0B (100%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-Select tweet_id  from Tweets where CHAR_LENGTH(content) > 15 ORDER BY tweet_id ; 
+#Select tweet_id  from Tweets where CHAR_LENGTH(content) > 15 ORDER BY tweet_id ;
+# i can also use length();
+Select tweet_id  from Tweets where LENGTH(content) > 15 ;
